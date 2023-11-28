b1c02d0e1e3252ed7c65a25b61c1d698036296fc
rdvieira <rodrigodenizads@gmail.com>
2023-11-27 21:05:39 -0300

    test: add find in OrderRepository

--- DDD-OrderRepository/src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ DDD-OrderRepository/src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -125,4 +125,41 @@ describe("Order repository test", () => {
       total: order.total(),
     });
   });
+
+  it("should find a order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const foundOrder = await orderRepository.find(order.id);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+    });
+
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: foundOrder.id,
+      customer_id: foundOrder.customerId,
+      total: foundOrder.total(),
+    });
+  });
 });
